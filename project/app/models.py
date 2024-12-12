b46d68b6c011a5c51915292aa164b0eed49b6cce
sreehari7306 <sreeharisreehari682@gmail.com>
2024-12-12 11:32:12 +0530

    hii

--- project/app/models.py
+++ project/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.models):
+class user(models.Model):
     name=models.TextField()
     phone=models.TextField()
     email=models.TextField()
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
-admin_username='admin'
-admin_pasw0rd='admin123'
+def userlogin(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        data=user.objects.all()
+        for i in data:
+            if i.email==email and i.password==password:
+                print('loggedin')
+                request.session['userlog']=i.email
+                return redirect(userhome)
+    return render(request,'user/userlogin.html')
+def register(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        username=request.POST['username']
+        password=request.POST['password']
+        data=user.objects.create(name=name,email=email,password=password)
+        data.save()
+    return render(request,'user/register.html')
 
+def userhome(request):
+    if 'userlog' in request.session:
+        return render(request,'user/userhome.html')
+    else:
+        return redirect(userlogin)
+
+def logout(requset):
+    if 'userlog' in requset.session:
+        del requset.session['userlog']
+        return redirect(userlogin)
+    else:
+        return redirect(userlogin)
